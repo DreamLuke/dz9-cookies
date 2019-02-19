9e658bf93f9d0c6441282acc59ea9106cc359d00
DreamLuke <36737163+DreamLuke@users.noreply.github.com>
2019-02-19 15:56:50 +0200

    Add files via upload

--- basket.php
+++ basket.php
@@ -7,53 +7,31 @@
 
 	if(empty($session->get('sum'))) {
 		$session->set('sum', 0);
+		$session->set('product1', 0);
+		$session->set('product2', 0);
+		$session->set('product3', 0);
+		$session->set('product4', 0);
+		$session->set('product5', 0);
 	}
 
 	if($id == 1) {
-		if(empty($session->get('product1'))) {
-			$session->set('product1', 1);
-			$session->set('sum', $session->increment('sum'));
-		} else {
-			$session->set('product1', $session->increment('product1'));
-			$session->set('sum', $session->increment('sum'));
-		}
+		$session->set('product1', $session->increment('product1'));
 	}
 	if($id == 2) {
-		if(empty($session->get('product2'))) {
-			$session->set('product2', 1);
-			$session->set('sum', $session->increment('sum'));
-		} else {
-			$session->set('product2', $session->increment('product2'));
-			$session->set('sum', $session->increment('sum'));
-		}
+		$session->set('product2', $session->increment('product2'));
 	}
 	if($id == 3) {
-		if(empty($session->get('product3'))) {
-			$session->set('product3', 1);
-			$session->set('sum', $session->increment('sum'));
-		} else {
-			$session->set('product3', $session->increment('product3'));
-			$session->set('sum', $session->increment('sum'));
-		}
+		$session->set('product3', $session->increment('product3'));
 	}
 	if($id == 4) {
-		if(empty($session->get('product4'))) {
-			$session->set('product4', 1);
-			$session->set('sum', $session->increment('sum'));
-		} else {
-			$session->set('product4', $session->increment('product4'));
-			$session->set('sum', $session->increment('sum'));
-		}
+		$session->set('product4', $session->increment('product4'));
 	}
 	if($id == 5) {
-		if(empty($session->get('product5'))) {
-			$session->set('product5', 1);
-			$session->set('sum', $session->increment('sum'));
-		} else {
-			$session->set('product5', $session->increment('product5'));
-			$session->set('sum', $session->increment('sum'));
-		}
+		$session->set('product5', $session->increment('product5'));
 	}
+
+	$sum = $session->get('product1') * 1 + $session->get('product2') * 2 + $session->get('product3') * 3 + $session->get('product4') * 4 + $session->get('product5') * 5;
+	$session->set('sum', $sum);
 
 	header('Location: ./shop.php');
 	exit;
